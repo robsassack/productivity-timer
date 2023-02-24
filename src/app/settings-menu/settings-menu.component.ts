1596e22bf17cc941294cdd3b28787360536ca217
Rob Sassack <rsassack25@gmail.com>
2023-02-24 00:55:25 -0500

    Added validation for input and resolved issue with closing menu while inputting time

--- src/app/settings-menu/settings-menu.component.ts
+++ src/app/settings-menu/settings-menu.component.ts
@@ -24,16 +24,50 @@ export class SettingsMenuComponent {
   }
 
   toggleSettingsMenu() {
+    // update times if menu gets closed
+    if (this.workTime >= 1 && this.breakTime >= 1 && this.longBreakTime >= 1) {
+      const times = {
+        work: this.workTime * 60,
+        break: this.breakTime * 60,
+        longBreak: this.longBreakTime * 60
+      };
+      this.settingsService.updateTime(times);
+    }
+    // close menu
     this.showSettingsMenu = !this.showSettingsMenu;
   }
 
-  updateTimes() {
+  updateTimes(event: Event) {
+    const input = event.target as HTMLInputElement;
+    // if input is zero or empty, do not update
+    if (input.value === '0' || input.value === '') {
+      return;
+    }
+
     const times = {
       work: this.workTime * 60,
       break: this.breakTime * 60,
       longBreak: this.longBreakTime * 60
     };
     this.settingsService.updateTime(times);
+  }
+
+  inputValidation(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    const value = input.value;
+    const keyPressed = event.key;
+
+    // prevent all negative numbers
+    if (keyPressed === '-') {
+      event.preventDefault();
+      return;
+    }
+
+    // do not allow decimals
+    if (keyPressed === '.') {
+      event.preventDefault();
+      return;
+    }
   }
 
   resetTimes() {
