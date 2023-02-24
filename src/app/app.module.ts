d8ed37b95f3022cd411d5b7e283779d3db8ddf7c
Rob Sassack <rsassack25@gmail.com>
2023-02-24 00:34:19 -0500

    Completed main functionality for time settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
@@ -12,7 +13,8 @@ import { SettingsMenuComponent } from './settings-menu/settings-menu.component';
     SettingsMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/settings-menu/settings-menu.component.ts
+++ src/app/settings-menu/settings-menu.component.ts
@@ -7,11 +7,36 @@ import { SettingsService } from '../settings.service';
   styleUrls: ['./settings-menu.component.css']
 })
 export class SettingsMenuComponent {
-  showSettingsMenu = true;
+  showSettingsMenu = false;
+
+  workTime!: number;
+  breakTime!: number;
+  longBreakTime!: number;
 
   constructor(private settingsService: SettingsService) {}
 
+  ngOnInit() {
+    this.settingsService.times$.subscribe(times => {
+      this.workTime = times.work / 60;
+      this.breakTime = times.break / 60;
+      this.longBreakTime = times.longBreak / 60;
+    });
+  }
+
   toggleSettingsMenu() {
     this.showSettingsMenu = !this.showSettingsMenu;
+  }
+
+  updateTimes() {
+    const times = {
+      work: this.workTime * 60,
+      break: this.breakTime * 60,
+      longBreak: this.longBreakTime * 60
+    };
+    this.settingsService.updateTime(times);
+  }
+
+  resetTimes() {
+    this.settingsService.updateTime({ work: 1500, break: 300, longBreak: 900 });
   }
 }
