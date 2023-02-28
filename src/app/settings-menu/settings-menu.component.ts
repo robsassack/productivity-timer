033b2184499adfab3c815882aab9c0abca24a4bb
Rob Sassack <rsassack25@gmail.com>
2023-02-28 18:59:03 -0500

    Changed settings to use ngModelChange

--- src/app/settings-menu/settings-menu.component.ts
+++ src/app/settings-menu/settings-menu.component.ts
@@ -5,7 +5,7 @@ import { sounds } from '../sounds';
 @Component({
   selector: 'app-settings-menu',
   templateUrl: './settings-menu.component.html',
-  styleUrls: ['./settings-menu.component.css']
+  styleUrls: ['./settings-menu.component.css'],
 })
 export class SettingsMenuComponent {
   showSettingsMenu = false;
@@ -15,57 +15,46 @@ export class SettingsMenuComponent {
   breakTime!: number;
   longBreakTime!: number;
 
-  soundList = sounds.map(sound => sound.name);
+  soundList = sounds.map((sound) => sound.name);
   selectedSound!: string;
 
   constructor(private settingsService: SettingsService) {}
 
   ngOnInit() {
-    this.settingsService.times$.subscribe(times => {
+    this.settingsService.times$.subscribe((times) => {
       this.workTime = times.work / 60;
       this.breakTime = times.break / 60;
       this.longBreakTime = times.longBreak / 60;
     });
 
-    this.settingsService.sound$.subscribe(sound => {
+    this.settingsService.sound$.subscribe((sound) => {
       this.selectedSound = sound;
     });
   }
 
   toggleSettingsMenu() {
-    // update times if menu gets closed
-    if (this.workTime >= 1 && this.breakTime >= 1 && this.longBreakTime >= 1) {
-      const times = {
-        work: this.workTime * 60,
-        break: this.breakTime * 60,
-        longBreak: this.longBreakTime * 60
-      };
-      this.settingsService.updateTime(times);
-    }
     // close menu
     this.showSettingsMenu = !this.showSettingsMenu;
   }
 
   updateTimes(event: Event) {
-    const input = event.target as HTMLInputElement;
-    // if input is zero or empty, do not update
-    if (input.value === '0' || input.value === '') {
+    if (Number(event) < 1 || Number(event) > 999 || String(event) === '') {
       return;
     }
 
     const times = {
       work: this.workTime * 60,
       break: this.breakTime * 60,
-      longBreak: this.longBreakTime * 60
+      longBreak: this.longBreakTime * 60,
     };
     this.settingsService.updateTime(times);
   }
 
   updateSound(event: Event) {
     const input = event.target as HTMLInputElement;
-    const sound = this.soundList.find(sound => sound === input.value);
+    const sound = this.soundList.find((sound) => sound === input.value);
     // get sound path from sounds array
-    const soundPath = sounds.find(sound => sound.name === input.value)?.path;
+    const soundPath = sounds.find((sound) => sound.name === input.value)?.path;
     this.playSound.src = soundPath!;
     if (soundPath) {
       this.playSound.load();
@@ -79,7 +68,12 @@ export class SettingsMenuComponent {
     const value = input.value;
     const keyPressed = event.key;
 
-    if (event.code === 'Backspace' || event.code === 'Delete' || event.code === 'ArrowLeft' || event.code === 'ArrowRight') {
+    if (
+      event.code === 'Backspace' ||
+      event.code === 'Delete' ||
+      event.code === 'ArrowLeft' ||
+      event.code === 'ArrowRight'
+    ) {
       return;
     }
 
