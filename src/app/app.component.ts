51fad897775226618fdcf09e5f0256cf0f9046d0
Rob Sassack <rsassack25@gmail.com>
2023-05-27 23:04:28 -0400

    Settings button now plays sound if setting is on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,18 @@ import { SettingsMenuComponent } from './settings-menu/settings-menu.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   @ViewChild(SettingsMenuComponent) settingsMenu!: SettingsMenuComponent;
 
   callToggleSettingsMenu() {
+    // play button sound if setting is enabled
+    if (this.settingsMenu.buttonSound) {
+      this.settingsMenu.playButtonSound.src = 'assets/button.wav';
+      this.settingsMenu.playButtonSound.play();
+    }
+
     this.settingsMenu.toggleSettingsMenu();
   }
 
